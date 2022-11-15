ebff0ca0683e0bdc45c5bc6b216ef50885c831b2
csanbar97 <cristianbs97@gmail.com>
2022-11-15 17:39:19 +0100

    corregido error con la condicional iniciar de user.php y admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if(!isset($_SESSION['iniciada'])&&$_SESSION['tipo']!='admin'){
+  if(!isset($_SESSION['iniciada'])||$_SESSION['tipo']!='admin'){
     header("Location: login.php?noiniciada");
   }
 
---
+++ user.php
@@ -0,0 +1,31 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['iniciada'])||$_SESSION['tipo']!='user'){
+    header("Location: login.php?noiniciada");
+  }
+
+  if(isset($_POST['submit'])){
+    header("Location: {$_POST['pagina']}.php");
+  }
+?>
+
+
+<?php
+  if($_COOKIE['contadorUser']==1){
+    echo "<h1>Bienvenido por primera vez, {$_SESSION['usuario']}</h1>";
+  }else{
+    echo "<h1>Hola de nuevo {$_SESSION['usuario']}</h1>";
+    echo "<h2>Tu última visita fue el {$_COOKIE['ultimaVisitaUser']}</h2>";
+    echo "<h2>Visitas: {$_COOKIE['contadorUser']}</h2>";
+  }
+
+?>
+
+<h3>¿Qué quieres hacer?</h3>
+<form action="user.php" method="post">
+  <select name="pagina" id="">
+    <option value="c">C</option>
+    <option value="d">D</option>
+  </select>
+  <input type="submit" value="Ir..." name="submit">
+</form>
